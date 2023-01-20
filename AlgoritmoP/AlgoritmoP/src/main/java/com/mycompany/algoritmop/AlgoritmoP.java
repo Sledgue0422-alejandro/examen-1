aef89f208c7354964e069f626d935e82e2374f44
Sledgue0422-alejandro <91093350+Sledgue0422-alejandro@users.noreply.github.com>
2023-01-19 23:28:59 -0500

    Add files via upload

---
+++ AlgoritmoP/AlgoritmoP/src/main/java/com/mycompany/algoritmop/AlgoritmoP.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.algoritmop;
+
+/**
+ *
+ * @author sebas_kxa9p6k
+ */
+public class AlgoritmoP {
+
+    public static void main(String[] args) {
+     //    Ventana x = new Ventana(); /*Set the Nimbus look and feel */
+       // x.setvisible (true);
+    }
+}
